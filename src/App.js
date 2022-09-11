d2435cce5c70d712206b7a5b57b29da3dfa0a9f4
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2022-09-11 20:09:30 +0100

    re-added missing logos, added dictionary component and search form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <a href="/" className="btn btn-primary">
-        Hello
-      </a>
+      <div className="container">
+        <header></header>
+        <Dictionary />
+      </div>
+      <footer></footer>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  const [word, setWord] = useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(`Searching for ${word} `);
+  }
+
+  function wordSearched(event) {
+    setWord(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={handleSubmit}>
+        <input type="text" autoComplete="off" onChange={wordSearched} />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+}
