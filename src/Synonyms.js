090b1a7f74484a680a36af2f45bbb5b7dbb7b7a2
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2022-09-26 15:51:44 +0100

    retrieved clicked on synonym value

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,12 +2,20 @@ import React from "react";
 import "./Synonyms.css";
 
 export default function Synonyms(props) {
+  function handleClick(event) {
+    console.log(event.currentTarget.id);
+  }
+
   if (props.synonyms) {
     return (
       <div className="synonyms">
         <ul>
           {props.synonyms.map((synonym, index) => {
-            return <li key={index}>{synonym}</li>;
+            return (
+              <li key={index} id={synonym} onClick={handleClick}>
+                {synonym}
+              </li>
+            );
           })}
         </ul>
       </div>
