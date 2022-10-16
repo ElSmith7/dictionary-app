f5cdf53ee0a3715923b32519856ce63ab013d9be
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2022-10-16 15:19:56 +0100

    touch ups of synonyms

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,22 +1,19 @@
-import React from "react";
 import "./Synonyms.css";
 
-
-  if (props.synonyms) {
-    return (
-      <div className="synonyms">
-        <ul>
-          {props.synonyms.map((synonym, index) => {
-            return (
-              <li key={index} id={synonym}>
-                {synonym}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
-  } else {
-    return null;
-  }
+if (props.synonyms) {
+  return (
+    <div className="synonyms">
+      <ul>
+        {props.synonyms.map((synonym, index) => {
+          return (
+            <li key={index} id={synonym}>
+              {synonym}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+} else {
+  return null;
 }
