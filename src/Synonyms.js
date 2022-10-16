2f837bfbe5bf164082674a365b6a587eb0b2eddf
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2022-10-16 15:21:21 +0100

    solved accidentlal deletion of function export

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,19 +1,22 @@
+import React from "react";
 import "./Synonyms.css";
 
-if (props.synonyms) {
-  return (
-    <div className="synonyms">
-      <ul>
-        {props.synonyms.map((synonym, index) => {
-          return (
-            <li key={index} id={synonym}>
-              {synonym}
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
-} else {
-  return null;
+export default function Synonyms(props) {
+  if (props.synonyms) {
+    return (
+      <div className="synonyms">
+        <ul>
+          {props.synonyms.map((synonym, index) => {
+            return (
+              <li key={index} id={synonym}>
+                {synonym}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
