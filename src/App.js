21abbda54c35edc5a70b79297cf5b310f30834ad
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2022-10-24 13:52:14 +0100

    Improved responsive css with max-width and displaying icon

--- src/App.js
+++ src/App.js
@@ -3,25 +3,28 @@ import Dictionary from "./Dictionary";
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <h1>
-          <i className="fa-solid fa-book-open" /> Dictionary
-        </h1>
-      </header>
-      <div className="container">
-        <Dictionary suggestedSearch="leaf" />
+    <div className="container">
+      <div className="App">
+        <header>
+          <h1>
+            <i className="fa-solid fa-book-open d-none d-md-inline" />{" "}
+            Dictionary
+          </h1>
+        </header>
+        <div className="container">
+          <Dictionary suggestedSearch="leaf" />
+        </div>
+        <footer>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/ElSmith7/react-dictionary-app"
+          >
+            <i className="fa-brands fa-github"></i> Open source code
+          </a>{" "}
+          by Eleanor Smith
+        </footer>
       </div>
-      <footer>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://github.com/ElSmith7/react-dictionary-app"
-        >
-          <i className="fa-brands fa-github"></i> Open source code
-        </a>{" "}
-        by Eleanor Smith
-      </footer>
     </div>
   );
 }
